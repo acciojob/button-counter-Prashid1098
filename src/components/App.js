be40e2075025f78ee893f8fb2cac810e28fdfd85
Prashid Pal <67044025+Prashid1098@users.noreply.github.com>
2024-02-08 22:41:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 
-import React from "react";
 import './../styles/App.css';
+import React,{useState} from "react";
+import ReactDOM from 'react-dom';
 
 const App = () => {
+    let [num,setNum]=useState(1);
+
+    function updateNum()
+    {
+        setNum(num+1);
+    }
+
+//  let [place,setPlace]=useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {num} times</p>
+        <button onClick={updateNum}>Click Me</button>
     </div>
   )
 }
-
 export default App
